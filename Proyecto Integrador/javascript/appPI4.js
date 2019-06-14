fd7086175e52ae0fada1b5421dcb8dc1b04f5d21
Federico Koch <fkoch@udesa.edu.ar>
2019-06-14 11:42:51 -0300

    cambios

--- Proyecto Integrador/javascript/appPI4.js
+++ Proyecto Integrador/javascript/appPI4.js
@@ -1,25 +1,27 @@
 window.onload = function(){
-  fetch("https://api.themoviedb.org/3/movie/popular?api_key=0c400c447b681b6753c82605b0973bd4&language=en-US&page=1")
+  var url = "https://api.themoviedb.org/3/search/movie?api_key=0c400c447b681b6753c82605b0973bd4&query=" + string + " &page=1&include_adult=true"
+
+  fetch(url)
     .then(function(respuesta) {
       return respuesta.json()
     })
     .then(function(informacion){
       console.log(informacion);
 
-      var arrayPeliculasPopulares = informacion.results
-      console.log(arrayPeliculasPopulares);
+      var arrayPeliculas = informacion.results
+      console.log(arrayPeliculas);
 
-      for (var i = 0; i < arrayPeliculasPopulares.length; i++) {
-        var id = arrayPeliculasPopulares[i].id
-        var titulo = arrayPeliculasPopulares[i].title
-        var url= arrayPeliculasPopulares[i].poster_path
+      for (var i = 0; i < arrayPeliculas.length; i++) {
+        var id = arrayPeliculas[i].id
+        var titulo = arrayPeliculas[i].title
+        var url2= arrayPeliculas[i].poster_path
 
         // <li>
         //     <img src="images/slider1.jpg" alt="">
         //     <div class="uk-position-center uk-panel"><h1>1</h1></div>
         // </li>
 
-        document.querySelector('ul.populares').innerHTML += "<li><img src='https://image.tmdb.org/t/p/w500" + url + "' alt=''><div class='uk-position-center uk-panel'></h2></div></li>"
+        document.querySelector('ul.peliculas').innerHTML += "<li><img src='https://image.tmdb.org/t/p/w500" + id + titulo + url2 +"' alt=''><div class='uk-position-center uk-panel'></h2></div></li>"
       }
     })
     .catch(function(error){
