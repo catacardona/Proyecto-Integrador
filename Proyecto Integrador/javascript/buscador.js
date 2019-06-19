0afe5706af177027b8d3147f7e0439f75f0c81df
Federico Koch <fkoch@udesa.edu.ar>
2019-06-19 12:22:43 -0300

    biemos

--- Proyecto Integrador/javascript/buscador.js
+++ Proyecto Integrador/javascript/buscador.js
@@ -10,5 +10,3 @@ window.addEventListener('load', function(){
   }
 
 })
-
-})
--- Proyecto Integrador/javascript/login.js
+++ Proyecto Integrador/javascript/login.js
@@ -1,8 +1,8 @@
 window.addEventListener("load",function() {
 
-  document.querySelector("form").addEventListener("submit",function(evento)) {
-    evento.preventDefault()
-    var name = document.querySelector("input[name='name']").value
+  document.querySelector("form.form").addEventListener("submit",function(evento) {
+    // evento.preventDefault()
+    var name = document.querySelector("input[name='nombre']").value
     console.log(name);
     sessionStorage.setItem("nombre",name)
     sessionStorage.getItem("nombre")
@@ -16,30 +16,29 @@ window.addEventListener("load",function() {
     sessionStorage.getItem("genero")
   })
 
+
+
 var arrayPelisFavoritas = []
 
 function agregarFavoritos(id) {
   alert("me clickearon!")
 
+  if (arrayPelisFavoritas.indexOf(id)===-1){
 
-
-if (arrayPelisFavoritas.indexOf(id)===-1){
-
-
-arrayPelisFavoritas.push(id)
-window.sessionStorage.setItem("favorita", JSON.stringify(arrayPelisFavoritas))
-
-})
-
-else {
-
-console.log(arrayPelisFavoritas.indexOf(id));
-  arrayPelisFavoritas.splice((arrayPelisFavoritas.indexOf(id),1)
-  console.log(arrayPelisFavoritas)
+  arrayPelisFavoritas.push(id)
   window.sessionStorage.setItem("favorita", JSON.stringify(arrayPelisFavoritas))
-}
+
+  } else {
+
+    console.log(arrayPelisFavoritas.indexOf(id));
+    arrayPelisFavoritas.splice((arrayPelisFavoritas.indexOf(id),1));
+    console.log(arrayPelisFavoritas)
+    window.sessionStorage.setItem("favorita", JSON.stringify(arrayPelisFavoritas))
+  }
 
   console.log(id);
   console.log(JSON.parse(window.sessionStorage.getItem("favorita")));
+
+}
 
 })
